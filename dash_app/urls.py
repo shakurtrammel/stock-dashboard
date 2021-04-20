e0ed6505daa596bb3110ad141bc6dd32eb22f272
Shakur Trammel <shakurPro@gmail.com>
2021-04-19 23:19:35 -0700

    Add User Auth UI

--- dash_app/urls.py
+++ dash_app/urls.py
@@ -3,6 +3,9 @@ from dash_app import views
 
 
 urlpatterns = [
+    path("dashboard", views.dashboard),
+    path("login", views.login),
+    path("register", views.register),
     path("quote", views.quote),
-    path("", views.home),
+    path("", views.index),
 ]
--- dash_app/views.py
+++ dash_app/views.py
@@ -1,17 +1,65 @@
 from django.shortcuts import HttpResponse, redirect, render
-from rest_framework import viewsets
+from django.contrib import messages
+from .models import User, Quote
 from .serializers import QuoteSerializer
-import requests
-from .models import Quote
+from rest_framework import viewsets
 from django.views.decorators.csrf import ensure_csrf_cookie
+import requests
+import bcrypt
 
 
-class QuoteView(viewsets.ModelViewSet):
-    serializer_class = QuoteSerializer
-    queryset = Quote.objects.all()
+
+# Registration & Login Controllers
+def register(request):
+    errors = User.objects.registration_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/")
+    else:
+        user = list(User.objects.filter(email=request.POST["email"]))
+        if len(user) == 0:
+            password_hash = bcrypt.hashpw(request.POST['rpassword'].encode(), bcrypt.gensalt()).decode()
+            User.objects.create(
+                fname=request.POST['fname'],
+                lname=request.POST['lname'],
+                email=request.POST['email'],
+                password=password_hash,
+            )
+            return redirect("/dashboard")
+        else:
+            return redirect("/")
 
 
-def home(request):
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/")
+    else: 
+        user = list(User.objects.filter(email=request.POST["lemail"]))
+        if len(user) == 0:
+            return HttpResponse("Email address not found. Please register to use app!")
+        else:
+            if user:
+                logged_user = user[0]
+                if bcrypt.checkpw(request.POST["lpassword"].encode(), logged_user.password.encode()):
+                    request.session["userid"] = logged_user.id
+                    return redirect("/dashboard")
+
+def logout(request):
+    request.session.flush()
+    return redirect("/")
+
+
+# Login Page View
+def index(request):
+    return render(request, 'index.html')
+
+
+# Dashboard Page View
+def dashboard(request):
     return redirect('http://localhost:3000')
     
 
@@ -38,3 +86,9 @@ def quote(request):
     update.close = data['close']
     update.save()
     return redirect('http://localhost:3000')
+
+
+# Backend API Viewsets
+class QuoteView(viewsets.ModelViewSet):
+    serializer_class = QuoteSerializer
+    queryset = Quote.objects.all()
