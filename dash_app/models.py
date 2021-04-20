f8185d9f48ce25e130fdeb525a679953b1cd1c72
Shakur Trammel <shakurPro@gmail.com>
2021-04-19 22:36:22 -0700

    Add User Auth Models

--- dash_app/models.py
+++ dash_app/models.py
@@ -1,4 +1,56 @@
 from django.db import models
+import re
+import bcrypt
+
+
+
+class UserManager(models.Manager):
+    def registration_validator(self, data):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if (len(data["fname"]) < 2) or (not data["fname"].isalpha()):
+            errors["fname"] = "First Name should be at least 2 characters and contain only letters."
+        if (len(data["lname"]) < 2) or (not data["lname"].isalpha()):
+            errors["lname"] = "Last Name should be at least 2 characters and contain only letters."
+        if not EMAIL_REGEX.match(data["email"]):             
+            errors["email"] = "Invalid email address."
+        if len(data["rpassword"]) < 8:
+            errors["rpassword"] = "Password should be at least 8 characters."
+        elif data["conf_pass"] != data["rpassword"]:
+            errors["conf_pass"] = "Passwords do not match."
+        return errors
+
+    def login_validator(self, data):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(data["lemail"]):             
+            errors["lemail"] = "Invalid email address."
+        else:
+            if len(data["lpassword"]) == 0:
+                errors["lpassword"] = "Password is required."
+            elif len(data["lpassword"]) < 8:
+                errors["lpassword"] = "Password should be at least 8 characters."
+        return errors
+
+
+class QuoteManager(models.Manager):
+    def wish_validator(self, data):
+        errors = {}
+        if len(data["search"]) == 0:
+            errors["search"] = "A stock symbol must be provided."
+        elif len(data["search"]) < 3:
+            errors["search"] = "A stock symbol must consist of at least 1 character."
+        return errors
+
+
+class User(models.Model):
+    fname = models.CharField(max_length=255)
+    fname = models.CharField(max_length=255)
+    fname = models.CharField(max_length=255)
+    fname = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    objects = UserManager()
 
 
 class Quote(models.Model):
@@ -19,3 +71,4 @@ class Quote(models.Model):
     close = models.DecimalField(max_digits=19, decimal_places=2, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = QuoteManager()
